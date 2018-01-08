a8d0bf3371fb2db800b47b7a651c58a8c02acb2f
Ruimeng Wang <34899888+RuimengWang@users.noreply.github.com>
2018-01-08 12:47:48 -0600

    Add files via upload

---
+++ download the file.R
@@ -0,0 +1,38 @@
+> install.packages("faraway")
+also installing the dependencies ¡®minqa¡¯, ¡®nloptr¡¯, ¡®Rcpp¡¯, ¡®RcppEigen¡¯, ¡®lme4¡¯
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/minqa_1.2.4.zip'
+Content type 'application/zip' length 667850 bytes (652 KB)
+downloaded 652 KB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/nloptr_1.0.4.zip'
+Content type 'application/zip' length 1173296 bytes (1.1 MB)
+downloaded 1.1 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/Rcpp_0.12.14.zip'
+Content type 'application/zip' length 4359254 bytes (4.2 MB)
+downloaded 4.2 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/RcppEigen_0.3.3.3.1.zip'
+Content type 'application/zip' length 2660603 bytes (2.5 MB)
+downloaded 2.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/lme4_1.1-15.zip'
+Content type 'application/zip' length 4739793 bytes (4.5 MB)
+downloaded 4.5 MB
+
+trying URL 'https://cran.rstudio.com/bin/windows/contrib/3.4/faraway_1.0.7.zip'
+Content type 'application/zip' length 775220 bytes (757 KB)
+downloaded 757 KB
+
+package ¡®minqa¡¯ successfully unpacked and MD5 sums checked
+package ¡®nloptr¡¯ successfully unpacked and MD5 sums checked
+package ¡®Rcpp¡¯ successfully unpacked and MD5 sums checked
+package ¡®RcppEigen¡¯ successfully unpacked and MD5 sums checked
+package ¡®lme4¡¯ successfully unpacked and MD5 sums checked
+package ¡®faraway¡¯ successfully unpacked and MD5 sums checked
+
+The downloaded binary packages are in
+C:\Users\wrm\AppData\Local\Temp\RtmpuWAoQe\downloaded_packages
+> library(faraway)
+> data(cheddar)
